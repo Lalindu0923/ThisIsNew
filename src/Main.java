c13e4a00dcf7e601aa04e63752d9beb1e539d72c
Lalindu0923 <lalindusjayasingha2002@gmail.com>
2026-06-05 15:21:15 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("This is a simple Java program.");
+        System.out.println("Java is a versatile programming language.");
+        System.out.println("Let's learn Java together!");
+        System.out.println("Java is widely used for web development.");
+        System.out.println("Java has a large community of developers.");
+        System.out.println("Java is known for its portability.");
+        System.out.println("Java is used in Android app development.");
+        System.out.println("Java is a great language for beginners.");
+        System.out.println("Java has a rich set of libraries and frameworks.");
+    }
+}
